63f6d8bca2971d84623b152b0b9f91eed28b0c3c
6Mouris9 <mourisworks@hotmail.com>
2026-04-20 14:16:35 -0300

    Front-end ajust

--- index.php
+++ index.php
@@ -2,25 +2,54 @@
 <html lang="en">
 
 <head>
-    <?php 
-    include_once("templates/default_head.php"); 
+    <?php
+    include_once("templates/default_head.php");
     ?>
     <title>Agenda Contato</title>
 </head>
 
 <body>
     <?php include_once("templates/header.php");
+
     if (isset($printMsg) && $printMsg != ''): ?>
-        <p id="msg"><?= $printMsg;?></p> //imprimir mensagem da sessao
-    <?php 
-    endif; 
+        <p id="msg"><?= $printMsg; ?></p> //imprimir mensagem da sessao
+    <?php
+    endif;
     ?>
+    <p id="msg">Testando mensagem</p>
     <h1 id="main-title">Minha Agenda</h1>
-        <?php if(count($contacts) > 0):?>
-            <p>TEM CONTATOS</p>
-        <?php else: ?>
-            <p id="empty-list-text">Ainda nao ha contatos na sua agenda, <a href=<?=BASE_URL;?>"/create.php">Clique aqui para adicionar</a></p>
-        <?php endif;?>
+    <?php if (count($contacts) > 0): ?>
+        <p>TEM CONTATOS</p>
+        <table class="table" id="contacts-table">
+            <thead>
+                <tr>
+                    <th escope="col">#</th>
+                    <th escope="col">Nome</th>
+                    <th escope="col">Numero</th>
+            
+                    <th escope="col" colspan="2"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($contacts as $contact): ?>
+                    <tr>
+                        <td scope="row" class="col-id"><?= $contact['id'] ?></td>
+                        <td scope="row"><?= $contact['name'] ?></td>
+                        <td scope="row"><?= $contact['number'] ?></td>
+                        
+                        <td class="actions">
+                            <a href="show.php?id=<?= $contact['id'] ?>"><i class="fas fa-eye check-icon"></i></a>
+                            <a href="edit.php?id=<?= $contact['id'] ?>"><i class="far fa-edit edit-icon"></i></a>
+                            <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p id="empty-list-text">Ainda nao ha contatos na sua agenda, <a href=<?= BASE_URL; ?>"/create.php">Clique aqui para
+                adicionar</a></p>
+    <?php endif; ?>
 
     <?= BASE_PATH; ?>
 </body>
---
+++ show.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include_once("templates/default_head.php");
+    ?>
+    <title>Agenda Contato</title>
+</head>
+
+<body>
+    <?php include_once("templates/header.php");
+    if (isset($printMsg) && $printMsg != ''): ?>
+        <p id="msg"><?= $printMsg; ?></p> //imprimir mensagem da sessao
+        <?php
+    endif;
+    ?>
+    <div class="container" id="view-contact-container">
+        <?php include_once("templates/backbtn.html");?>
+        <h1 id="main-title"><?= $contact['name'];?></h1>
+        <p class="bold">Telefone:</p>
+        <p><?=$contact['number'];?></p>
+        <p class="bold">Observacoes:</p>
+        <p><?=$contact['observations'];?></p>
+    </div>
+    
+</body>
+
+</html>
